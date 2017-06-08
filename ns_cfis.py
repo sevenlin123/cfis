566dd3b25d70a7012577ddf9a41605290967cd5f
Edward Lin <edward.lin@me.com>
2017-06-08 11:23:32 +0800

    Added PS1 catalog

--- ns_cfis.py
+++ ns_cfis.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.ensemble import IsolationForest as IF
 from sklearn.metrics import confusion_matrix, roc_curve
 from sklearn.model_selection import train_test_split
+from scipy.spatial import cKDTree as KDTree
 import pandas as pd
 import seaborn as sns
 import os, sys
@@ -18,10 +19,11 @@ class fits_cat:
         self.stationary0 = self.catalog[self.catalog['MATCHES'] > 0]
         self.stationary1 = self.catalog[self.catalog['MATCHES'] > 1]
         self.stationary2 = self.catalog[self.catalog['MATCHES'] > 2]
-        self.ns0 = self.catalog[logical_and(self.catalog['MATCHES'] == 0, self.catalog['OVERLAPS'] >1)]
-        self.ns1 = self.catalog[logical_and(self.catalog['MATCHES'] == 0, self.catalog['OVERLAPS'] >2)]
-        self.real_feature = self.feature(self.stationary2)
-        self.ns_feature = self.feature(self.ns1)
+        self.ns0 = self.catalog[logical_and(logical_and(self.catalog['MATCHES'] == 0, self.catalog['OVERLAPS'] >0), self.catalog['MAG_PSF']<24)]
+        self.ns1 = self.catalog[logical_and(self.catalog['MATCHES'] == 0, self.catalog['OVERLAPS'] >1)]
+        self.ns2 = self.catalog[logical_and(self.catalog['MATCHES'] == 0, self.catalog['OVERLAPS'] >2)]
+	self.real_feature = self.feature(self.stationary2)
+        self.ns_feature = self.feature(self.ns0)
     def plot(self):
         #plt.xlim(165.5, 168.0)
         #plt.ylim(31.5, 32.0)
@@ -68,36 +70,64 @@ class IF_real_bogus:
         result = self.IFmod.predict(self.feature)
         self.normal = (result == 1)
         self.abnormal = (result == -1)
-        print "total:{0}, normal:{1}, abnormal:{2}, total/normal:{3}".format(len(self.feature), 
+        print "total:{0}, normal:{1}, abnormal:{2}, normal/total:{3}".format(len(self.feature), 
             	self.normal.sum(), self.abnormal.sum(), self.normal.sum()/float(len(self.feature)))
                     
 def build_model(fits_tab):
 	sources = fits_cat(fits_tab)
-	print len(sources.stationary2), len(sources.ns1)
+	#print len(sources.stationary2), len(sources.ns1)
 	real_bogus = IF_real_bogus(sources.real_feature)
 	real_bogus.train()
 	real_bogus.validation()
 	return real_bogus.IFmod
 
+def remove_ps1_source(ps1_cat, ns_cat):
+	ps1 = fits.open(ps1_cat)[1].data
+	ps1_ra = ps1['f0']
+	ps1_dec = ps1['f1']
+	ra = ns_cat['X_WORLD']
+	dec = ns_cat['Y_WORLD']
+	ns_tree = KDTree(zip(ra, dec))
+	ps1_tree = KDTree(zip(ps1_ra, ps1_dec))
+	match = ns_tree.query_ball_tree(ps1_tree, r=.5/3600.)
+	#print len(match), len(ra), len(ps1)
+	ps1_match = []
+	for i in match:
+		if i != []:
+			ps1_match.append(True)
+		else:
+			ps1_match.append(False)
+	return array(ps1_match)
+	
+
 def ns_cat(fits_tab):
-	#try:
-	sources = fits_cat(fits_tab)
-	print fits_tab
-	real_bogus = IF_real_bogus(sources.ns_feature)
-	real_bogus.IFmod = RB_model
-	real_bogus.validation()
-	ns_cat = sources.ns1[real_bogus.normal]
-	fits.writeto('{}.ns'.format(fits_tab), ns_cat, header=sources.header)
-	#except ValueError:
-		#print 'error: {0}'.format(fits_tab)
+	try:
+		sources = fits_cat(fits_tab)
+		healpix = fits_tab.split('/')[-1].rstrip('_cat.fits') #HPX_03205_RA_194.1_DEC_+28.6_cat.fits
+		print fits_tab
+		real_bogus = IF_real_bogus(sources.ns_feature)
+		real_bogus.IFmod = RB_model
+		real_bogus.validation()
+		ns_cat = sources.ns0[real_bogus.normal]
+		ps1_cat = '{0}{1}_STK.fits'.format(ps1_dir, healpix) #HPX_02298_RA_164.5_DEC_+38.7_STK.fits
+		ps1_match = remove_ps1_source(ps1_cat, ns_cat)
+		ns_cat = ns_cat[~ps1_match]
+		#print len(ps1_match), ps1_match.sum()
+		print "PS1 remove {0} stationary sources".format(ps1_match.sum())
+		print "{0} non-stationary sources remain".format((~ps1_match).sum())
+		fits.writeto('{}.ns'.format(fits_tab), ns_cat, header=sources.header)
+	except ValueError:
+		print 'error: {0}'.format(fits_tab)
 	
 def main():
 	global RB_model
-	workdir = '/sciproc/disk2/cfis/catalog_20170419/'
+	global ps1_dir
+	ps1_dir = '/sciproc/disk2/cfis/ps_pv3_STK_hp/'
+	workdir = '/sciproc/disk2/cfis/catalog_20170501/'
 	fits_list = filter(lambda x: x.endswith('fits'), os.listdir(workdir))
 	fits_list = [workdir + i for i in fits_list]
-	fits_list = ['/sciproc/disk2/cfis/catalog_20170419/HPX_02559_RA_178.6_DEC_+35.7_cat.fits']
-	training_fits = 'HPX_02937_RA_160.3_DEC_+31.4_cat.fits'
+	#fits_list = ['/sciproc/disk2/cfis/catalog_20170501/HPX_02559_RA_178.6_DEC_+35.7_cat.fits']
+	training_fits = 'HPX_03071_RA_178.6_DEC_+30.0_cat.fits'
 	RB_model = build_model(workdir + training_fits)
 	map(ns_cat, fits_list)
 	
